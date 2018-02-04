e11f2e8d1c3e762e7c046ebfe90eb9475cdc7190
nshehzad <nshehzad@deloitte.com>
2018-02-03 23:45:39 -0500

    <commit message>

--- src/test/java/rdemo/HelloControllerTest.java
+++ src/test/java/rdemo/HelloControllerTest.java
@@ -4,7 +4,6 @@ import static org.hamcrest.Matchers.equalTo;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
