e561b1a52a9ce3b29b199f3847e6e39771d9aa86
nshehzad <nshehzad@deloitte.com>
2018-02-16 14:34:57 -0500

    failed unit test

--- src/test/java/rdemo/HelloControllerTest.java
+++ src/test/java/rdemo/HelloControllerTest.java
@@ -28,7 +28,7 @@ public class HelloControllerTest {
     public void getHello() throws Exception {
         mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(content().string(equalTo("Greetings from my Spring Boots!")));
+                .andExpect(content().string(equalTo("Greetings from Demo Spring Boots!")));
     }
 }
 
