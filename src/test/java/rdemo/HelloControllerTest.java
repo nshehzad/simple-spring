cdf0a8b7e69e387aeb16d060667ea373b655fedc
nshehzad <nshehzad@deloitte.com>
2018-02-15 21:06:56 -0500

    demo code

--- src/test/java/rdemo/HelloControllerTest.java
+++ src/test/java/rdemo/HelloControllerTest.java
@@ -22,10 +22,15 @@ public class HelloControllerTest {
     @Autowired
     private MockMvc mvc;
 
+
+
     @Test
     public void getHello() throws Exception {
         mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(content().string(equalTo("Greetings from my Spring Boots!")));
+                .andExpect(content().string(equalTo("Greetings from DEVOps Spring Boots!")));
     }
 }
+
+
+
